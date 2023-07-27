1d7abf4edc88cf6af2758c77c1504808f9d6d951
ihtier <ihtier_@outlook.com>
2023-07-27 22:07:15 +0300

    add OpenXR dependency

---
+++ cmake/FindOpenXR.cmake
@@ -0,0 +1,31 @@
+#
+# In:
+#   OPENXR_ROOT 
+#
+# Out:
+#   OPENXR_FOUND
+#   OPENXR_LIBRARY
+#   OPENXR_INCLUDE_DIRS
+
+find_library(
+  OPENXR_LIBRARY
+  NAMES openxr_loader
+  PATHS ${OPENXR_ROOT}/lib)
+
+find_path(
+  OPENXR_INCLUDE_DIR openxr/openxr.h
+  PATHS ${OPENXR_ROOT}/include)
+
+if(OPENXR_LIBRARY STREQUAL "OPENXR_LIBRARY-NOTFOUND" OR
+   OPENXR_INCLUDE_DIR STREQUAL "OPENXR_LIBRARY-NOTFOUND")
+  set(OPENXR_FOUND FALSE)
+else()
+  set(OPENXR_FOUND TRUE)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  OpenXR
+  DEFAULT_MSG
+  OPENXR_LIBRARY
+  OPENXR_INCLUDE_DIR)
---
+++ cmake/Packages.cmake
@@ -0,0 +1,1 @@
+find_package(OpenXR REQUIRED)
